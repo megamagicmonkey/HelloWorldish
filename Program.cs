e20f4f6dc6e893f0b54d22dc1850fe7e1a463515
megamagicmonkey <megamagicmonkey@gmail.com>
2021-08-18 13:56:13 -0500

    It's kinda like hello world?

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! I am the bridge keeper! Let us see if you are worthy to pass!");
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            if (name != "No" || name != "I don't know"){
+                Console.WriteLine("What is your favorite color?");
+                var color = Console.ReadLine();
+                if (color != "No" || color != "I don't know"){
+                    Console.WriteLine("What is your quest?");
+                    var quest = Console.ReadLine();
+                    if (quest != "No" || quest != "I don't know"){
+                        Console.WriteLine("So your name is " + name +", your favorite color is " + color + ", and your oh-so important quest is " + quest +"?");
+                        Console.WriteLine("Are you sure?");
+                        var certain = Console.ReadLine();
+                        if (certain != "No" || certain != "N"){
+                            Console.WriteLine("Hello Worl- Urk! I mean, you may pass, adventurer.");
+                            
+                        }
+                    }
+                }
+            }
+            else{
+                Console.WriteLine("*you are flung into the abyss below the bridge*");
+            }
+        }
+    }
+}
