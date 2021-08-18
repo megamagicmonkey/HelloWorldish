93413d5abd4b4b0362aab5e15b6d7e7f5b3c30ce
megamagicmonkey <megamagicmonkey@gmail.com>
2021-08-18 14:25:38 -0500

    Corrected the bridge keeper's logic

--- Program.cs
+++ Program.cs
@@ -9,17 +9,17 @@ namespace HelloWorld
             Console.WriteLine("Hello! I am the bridge keeper! Let us see if you are worthy to pass!");
             Console.WriteLine("What is your name?");
             var name = Console.ReadLine();
-            if (name != "No" || name != "I don't know"){
+            if (name != "No" && name != "I don't know"){
                 Console.WriteLine("What is your favorite color?");
                 var color = Console.ReadLine();
-                if (color != "No" || color != "I don't know"){
+                if (color != "No" && color != "I don't know"){
                     Console.WriteLine("What is your quest?");
                     var quest = Console.ReadLine();
-                    if (quest != "No" || quest != "I don't know"){
+                    if (quest != "No" && quest != "I don't know"){
                         Console.WriteLine("So your name is " + name +", your favorite color is " + color + ", and your oh-so important quest is " + quest +"?");
                         Console.WriteLine("Are you sure?");
                         var certain = Console.ReadLine();
-                        if (certain != "No" || certain != "N"){
+                        if (certain != "No" && certain != "N"){
                             Console.WriteLine("Hello Worl- Urk! I mean, you may pass, adventurer.");
                             
                         }
